59c66f9561066b43719984504f43efd009d2f5b9
Ben Walton <bdwalton@gmail.com>
2009-02-16 21:24:55 -0500

    enable version specific init; add mode init stuff from old setup; add keybindings

---
+++ _emacs.d/modes/mail.el
@@ -0,0 +1,12 @@
+;; we want to turn on mail-mode for sup files.
+
+;; mail-mode runs text-mode first.
+;; make sure we use fill mode and turn on colors
+(add-to-list 'auto-mode-alist
+	     '("sup\\.\\(compose\\|forward\\|reply\\|resume\\)-mode$" . mail-mode))
+
+;; we want colours and text-wrapping
+(add-hook 'mail-mode-hook (function (lambda ()
+                                      (auto-fill-mode 1)
+				      (flyspell-mode 1))
+ 				    ))
