299b1d2576d9d1dd521d655b1e1397f76fbfc009
Ben Walton <bwalton@artsci.utoronto.ca>
2009-02-20 15:36:40 -0500

    allow sgml/xml to set variables for indentation.

--- _emacs.d/general.el
+++ _emacs.d/general.el
@@ -16,3 +16,11 @@
 ;; enable a few features that we like
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+;; allow sgml/xml files to set indentation variables, etc.
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((sgml-indent-step . 1) (sgml-indent-data . 1)))))
