88d44a2567baba5f0290eb31a8c833acd03c40f5
Ben Walton <bwalton@artsci.utoronto.ca>
2009-05-20 15:03:19 -0400

    added macros keybindings
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- _emacs.d/keybindings.el
+++ _emacs.d/keybindings.el
@@ -6,3 +6,8 @@
 ;; make easier alt-x (when ctrl is bound to caps lock)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; macros with 1 key
+(global-set-key [f4]  'start-kbd-macro)
+(global-set-key [f5]  'end-kbd-macro)
+(global-set-key [f6]  'call-last-kbd-macro)
