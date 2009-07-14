1114d62ba49522bda594002789e46041539e7fd5
Ben Walton <bwalton@artsci.utoronto.ca>
2009-07-14 14:10:45 -0400

    Custom: allow a few sgml variables
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -75,3 +75,15 @@
 
 ;; finally, always start with ~/ as the current directory
 (cd (getenv "HOME"))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((sgml-parent-document . "../machines.xml") (sgml-indent-step . 1) (sgml-indent-data . 1)))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
