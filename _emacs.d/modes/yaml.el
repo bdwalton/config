e60dc93190aea40a7056821aa3b3333f0583820e
Ben Walton <bdwalton@gmail.com>
2009-08-12 15:29:48 -0400

    make yaml mode autoload instead of using require
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- _emacs.d/modes/yaml.el
+++ _emacs.d/modes/yaml.el
@@ -1,4 +1,4 @@
-(require 'yaml-mode)
+(autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-hook 'yaml-mode-hook
 	  '(lambda ()
