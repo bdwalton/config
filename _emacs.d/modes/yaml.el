7263827eb110e90f6b3868b971c85d689b57fe58
Ben Walton <bwalton@artsci.utoronto.ca>
2009-08-26 10:22:04 -0400

    .yml _and_ .yaml file extentions trigger yaml-mode
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- _emacs.d/modes/yaml.el
+++ _emacs.d/modes/yaml.el
@@ -1,5 +1,5 @@
 (autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(yml\\|yaml\\)$" . yaml-mode))
 (add-hook 'yaml-mode-hook
 	  '(lambda ()
 	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
