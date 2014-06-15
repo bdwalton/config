3321029c206181f694d881b8c78d4e7693b1c767
Ben Walton <bdwalton@gmail.com>
2014-06-15 10:50:58 +0100

    nuke the useless per-distro emacs settings
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- _emacs.d/distro/ubuntu.el
+++
@@ -1,7 +0,0 @@
-;; the git stuff doesn't happen automatically in ubuntu.
-(add-to-list 'load-path "/usr/share/doc/git-core/contrib/emacs/")
-(load-library "git.el")
-(load-library "git-blame.el")
-(load-library "vc-git.el")
-(add-to-list 'vc-handled-backends 'GIT)
-
