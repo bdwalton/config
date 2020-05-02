db8e0ad42a2f86f83b9a91fa0a79738d80b2d890
Ben Walton <bdwalton@gmail.com>
2020-05-02 15:47:46 +0100

    emacs: drop even more old cruft
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- dotfiles/_emacs.d/modes/yaml.el
+++
@@ -1,5 +0,0 @@
-(autoload 'yaml-mode "yaml-mode" "Major mode for editing YAML files." t)
-(add-to-list 'auto-mode-alist '("\\.\\(yml\\|yaml\\)$" . yaml-mode))
-(add-hook 'yaml-mode-hook
-	  '(lambda ()
-	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
