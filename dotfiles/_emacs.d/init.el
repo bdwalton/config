7f8b3f88efc0b91612b17e1c2b1c2029b4e01ebe
Ben Walton <bdwalton@gmail.com>
2022-02-12 14:58:40 +0000

    emacs: update elpa packages to fix broken python mode interaction with swiper/ivy
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -44,14 +44,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes '(zenburn))
  '(custom-safe-themes
-	 (quote
-		("19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" default)))
- )
+   '("9e3ea605c15dc6eb88c5ff33a82aed6a4d4e2b1126b251197ba55d6b86c610a1" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" default))
+ '(package-selected-packages '(go-mode zenburn-theme swiper rust-mode))
+ '(safe-local-variable-values '((sgml-indent-step . 1) (sgml-indent-data . 1))))
 
 ;; now pull in the optional site-local config
 (setq site-local-lib (concat "emacs-" (getenv "BDW_CONFIG_TYPE") ".el"))
 (if (locate-library site-local-lib)
     (load-library site-local-lib)
   (print "Not loading site local library."))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
