9dd9a39a3e3d6717de81e7edcb4932ef79555bd4
Ben Walton <bdwalton@gmail.com>
2022-08-16 20:17:54 +0100

    emacs: drop some legacy 'custom-set-variables'
    They were being overridden anyway, despite my past warning to
    myself. We haven't used xml/sgml in so long there is no need to merge
    this back into the top-level custom-set-variables again.

--- dotfiles/_emacs.d/lisp/general.el
+++ dotfiles/_emacs.d/lisp/general.el
@@ -2,14 +2,6 @@
 (require 'midnight)
 (midnight-delay-set 'midnight-delay "4:30am")
 
-;; allow sgml/xml files to set indentation variables, etc.
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((sgml-indent-step . 1) (sgml-indent-data . 1)))))
-
 (column-number-mode t)                   ;; show column numbers
 ;; --- end of djcb's tips
 
