2223de5743c76b0ed1004f5c19791f5ea45a2749
Ben Walton <bdwalton@gmail.com>
2022-08-16 19:53:32 +0100

    emacs: move most keybindings back to init.el

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -42,6 +42,18 @@
 (set-face-background 'hl-line "#3e4446")
 (set-face-background 'highlight "#3e4446")
 
+;; General keybindings here
+
+(global-set-key "\M-g" 'goto-line)
+;; make easier alt-x (when ctrl is bound to caps lock)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+;; macros with 1 key
+(global-set-key [f4]  'start-kbd-macro)
+(global-set-key [f5]  'end-kbd-macro)
+(global-set-key [f6]  'call-last-kbd-macro)
+;; Mostly for coding modes, but make it global.
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
 
 ;; Now configure the packages we want
 (use-package counsel)
--- dotfiles/_emacs.d/lisp/keybindings.el
+++ dotfiles/_emacs.d/lisp/keybindings.el
@@ -1,23 +1,2 @@
-;; save a few keystrokes jumping to specific lines.
-(global-set-key "\M-g" 'goto-line)
-
-;; these were taken from: http://steve.yegge.googlepages.com/effective-emacs
-
-;; make easier alt-x (when ctrl is bound to caps lock)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
-
-;; macros with 1 key
-(global-set-key [f4]  'start-kbd-macro)
-(global-set-key [f5]  'end-kbd-macro)
-(global-set-key [f6]  'call-last-kbd-macro)
-
 ;; we do this fairly often, bind it to something easy.
 (global-set-key [f9] 'my-indent-all)
-
-;; always kill the current buffer, prompting if unsaved, rather than
-;; asking for the buffer name.
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-;; Mostly for coding modes, but make it global.
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
