76f9f84a8449b7a7f90d1062efb54ffa9a6da693
Ben Walton <bdwalton@gmail.com>
2022-08-16 21:39:11 +0100

    emacs: custom-set-faces gets put back, so restore it
    Put it right next to the custom-set-variables though.

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -108,6 +108,13 @@
  '(package-selected-packages
    '(counsel editorconfig go-mode magit swiper rust-mode use-package which-key zenburn-theme)))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; now pull in the optional site-local config
 (setq site-local-lib
       (concat
