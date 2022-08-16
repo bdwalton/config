b86e2b7e19b2e56bc552f97af52897c828824acc
Ben Walton <bdwalton@gmail.com>
2022-08-16 18:34:37 +0100

    emacs: Ensure the use-package package is always available
    Load it by default as we're going to move more config to use that
    instead of bespoke seutp.

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -9,6 +9,11 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 
 (defconst user-init-dir
   (expand-file-name
