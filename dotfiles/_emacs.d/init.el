41f62b4f9971a4d451bf4a14e816a1e0349ecb54
Ben Walton <bdwalton@gmail.com>
2022-08-17 22:07:28 +0100

    emacs: switch to theme doom-zenburn (using doom-theme package)
    This gives better integration with other modes and packages than the
    default theme but is still nice and easy on the eyes.

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -107,7 +107,12 @@
 
 (use-package rust-mode)
 
-(use-package zenburn-theme)
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t    ;; if nil, bold is universally disabled
+	doom-themes-enable-italic t) ;; if nil, italics is universally disabled
+  (load-theme 'doom-zenburn t)
+  (doom-themes-visual-bell-config))  ;; Enable flashing mode-line on errors
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -117,11 +122,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(zenburn))
- '(custom-safe-themes
-   '("a3e99dbdaa138996bb0c9c806bc3c3c6b4fd61d6973b946d750b555af8b7555b" default))
  '(package-selected-packages
-   '(ivy-rich rainbow-delimiters diminish counsel editorconfig go-mode magit swiper rust-mode use-package which-key zenburn-theme)))
+   '(doom-themes ivy-rich rainbow-delimiters diminish counsel editorconfig go-mode magit swiper rust-mode use-package which-key)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
