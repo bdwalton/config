6b316e8e61fe95bcda61992faa5f8de2c7655ed0
Ben Walton <bdwalton@gmail.com>
2023-02-12 18:58:37 +0000

    emacs: Add a preamble comment
    Now that we auto-generate init.el, add the reminder as a header comment.

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -1,3 +1,8 @@
+;; This is bdwalton's emacs config. It is auto-generated from init.org.
+;; Local changes will be overwritten.
+;;
+;;
+
 ;; Wire up straight.el; We use that instead of package. Note that
 ;; early-init.el should be force disabling package as well.
 (defvar bootstrap-version)
