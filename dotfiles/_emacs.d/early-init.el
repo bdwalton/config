15f04157866ba1caf56cfd412fa24ee8692c5a8d
Ben Walton <bdwalton@gmail.com>
2023-02-12 20:01:22 +0000

    emacs: Teach init.org to manage early-init.el too
    Have init.org tangle output to both init.el and early-init.el. This
    makes the config more literate by having it all in one org file.

--- dotfiles/_emacs.d/early-init.el
+++ dotfiles/_emacs.d/early-init.el
@@ -1,2 +1,5 @@
+;; This is bdwalton's emacs config. It is auto-generated from init.org.
+;; Local changes will be overwritten.
+
 ;; Ensure that package isn't initialized before we get to straight.el
 (setq package-enable-at-startup nil)
