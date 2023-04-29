e10847fcf820a0eed7758ef9655b6b95c4646900
Ben Walton <bdwalton@gmail.com>
2023-04-29 19:15:58 +0100

    emacs: Change font and default window size in sway scratchpad
    Adjusting to the newer hidpi handling, we change font face to a) use
    Liberation Mono which is used by sway and b) set a smaller
    size. Additionally, we have the scratchpad window use a smaller size
    now too.

--- dotfiles/_emacs.d/init.el
+++ dotfiles/_emacs.d/init.el
@@ -28,7 +28,7 @@
            ;; If you edit it by hand, you could mess it up, so be careful.
            ;; Your init file should contain only one such instance.
            ;; If there is more than one, they won't work right.
-           '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 218 :width normal)))))
+           '(default ((t (:family "Liberation Mono" :foundry "PfEd" :slant normal :weight normal :height 109 :width normal)))))
           (scroll-bar-mode -1)
           (tool-bar-mode -1)))))
 
