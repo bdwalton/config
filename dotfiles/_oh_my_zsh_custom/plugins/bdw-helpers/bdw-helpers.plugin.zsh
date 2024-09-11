1e6da9701691dddfdfba357eb3dd9e240b3703d0
Ben Walton <bdwalton@gmail.com>
2024-09-11 16:57:39 +0100

    zsh: add k() function helper to my set
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

--- dotfiles/_oh_my_zsh_custom/plugins/bdw-helpers/bdw-helpers.plugin.zsh
+++ dotfiles/_oh_my_zsh_custom/plugins/bdw-helpers/bdw-helpers.plugin.zsh
@@ -16,6 +16,11 @@ function lca() {
     ls -h -l -A --color=yes $@ | less
 }
 
+func k() {
+  knock -d100 "${1}" 20000:udp 28118:udp 19005:udp && \
+    ssh "${1}"
+}
+
 # Launch a new titled tmux window that ssh's to the designated host We
 # force the use of our shell in what should avoid login mode (which
 # would trigger)
